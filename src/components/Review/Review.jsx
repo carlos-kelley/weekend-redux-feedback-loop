59f657118fc7d5d96df2c3ca0201c450b54e99f9
Carlos Kelley <carloskelley@protonmail.com>
2022-06-15 21:53:32 -0500

    dispatches working. the display isnt right, need to make a number not a string

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -2,51 +2,56 @@ import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Review() {
-    //   const [feeling, setFeeling] = useState(0);
-    //   const dispatch = useDispatch();
-    //   const history = useHistory();
+  const feeling = useSelector(
+    (store) => store.feelingReducer
+  );
+  const understanding = useSelector(
+    (store) => store.understandingReducer
+  );
+  const support = useSelector(
+    (store) => store.supportReducer
+  );
+  const comments = useSelector(
+    (store) => store.commentsReducer
+  );
+  //   const [feeling, setFeeling] = useState(0);
+  //   const dispatch = useDispatch();
+  //   const history = useHistory();
 
-    //   const feelingChange = () => {
-    //     setFeeling(event.target.value);
-    //   };
+  //   const feelingChange = () => {
+  //     setFeeling(event.target.value);
+  //   };
 
-    //   const sendFeeling = () => {
-    //     dispatch({
-    //       type: "SEND_FEELING",
-    //       payload: feeling,
-    //     });
-    //     console.log(feeling);
-    //     if (
-    //       feeling !== null &&
-    //       feeling > 0 &&
-    //       feeling < 6
-    //     ) {
-    //       history.push("/understanding");
-    //     } else {
-    //       alert(
-    //         "Please enter a number between 1 and 5"
-    //       );
-    //     }
-    //   };
+  //   const sendFeeling = () => {
+  //     dispatch({
+  //       type: "SEND_FEELING",
+  //       payload: feeling,
+  //     });
+  //     console.log(feeling);
+  //     if (
+  //       feeling !== null &&
+  //       feeling > 0 &&
+  //       feeling < 6
+  //     ) {
+  //       history.push("/understanding");
+  //     } else {
+  //       alert(
+  //         "Please enter a number between 1 and 5"
+  //       );
+  //     }
+  //   };
 
-    return (
-        <div>
-            <h1>Review Your Feedback</h1>
-            <p>
-                Feelings:
-            </p>
-            <p>
-                Understanding:
-            </p>
-            <p>
-                Support:
-            </p>
-            <p>
-                Comments:
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Review Your Feedback</h1>
+      <p>Feelings: {feeling}</p>
+      <p>Understanding: {understanding}</p>
+      <p>Support: {support}</p>
+      <p>Comments: {comments}</p>
+    </div>
+  );
 }
 export default Review;
